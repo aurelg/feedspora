04911fb3028b6b98c7e4245eecd7bee8cefc852c
Aurélien Grosdidier <aurelien.grosdidier@latitude77.org>
2015-11-26 14:09:30 +0100

    Added reveal_readlling

---
+++ TODO.md
@@ -0,0 +1,3 @@
+- Fix Facebook support
+- Add support for [LinkedIn](http://linkedin.com/) with [python-linkedin](https://github.com/ozgur/python-linkedin)
+
---
+++ src/feedspora/reveal_reallink.py
@@ -0,0 +1,27 @@
+'''
+Created on Nov 26, 2015
+@author: aurelien
+
+
+This script modifies an Atom feed (given as parameter) by replacing every entries' links and ID
+by the first link found in its content. The modified feed is printed on stdout. It allows the
+new modified feed to make the "real" link available, instead of the default link, when the latter
+is not relevant (see Atom feeds generated out of Diaspora).
+'''
+
+from bs4 import BeautifulSoup
+import sys
+
+srcfilename = sys.argv[1]
+try:
+    with open(srcfilename) as srcfile:
+        soup = BeautifulSoup(srcfile, 'html.parser')
+except FileNotFoundError:
+    print("File "+srcfilename+ " not found.")
+
+for entry in soup.find_all('entry'):
+    content_soup = BeautifulSoup(entry.find('content').string, 'html.parser')
+    entry.find('link')['href'] = content_soup.find('a')['href']
+    entry.find('id').string = content_soup.find('a')['href']
+
+print(soup)
