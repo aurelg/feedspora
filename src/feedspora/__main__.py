f0428e9fec0ce976c5e5a537d5cd1cc8f63fc776
Zi1mo5zo <aurelien.grosdidier+git@gmail.com>
2017-12-03 09:15:14 +0100

    Linting

--- src/feedspora/__main__.py
+++ src/feedspora/__main__.py
@@ -9,10 +9,10 @@ from feedspora.feedspora_runner import FeedSpora
 from feedspora.feedspora_runner import DiaspyClient  # @UnusedImport
 from feedspora.feedspora_runner import TweepyClient  # @UnusedImport
 from feedspora.feedspora_runner import FacebookClient  # @UnusedImport
-from feedspora.feedspora_runner import WPClient # @UnusedImport
-from feedspora.feedspora_runner import MastodonClient # @UnusedImport
-from feedspora.feedspora_runner import ShaarpyClient # @UnusedImport
-from feedspora.feedspora_runner import LinkedInClient # @UnusedImport
+from feedspora.feedspora_runner import WPClient  # @UnusedImport
+from feedspora.feedspora_runner import MastodonClient  # @UnusedImport
+from feedspora.feedspora_runner import ShaarpyClient  # @UnusedImport
+from feedspora.feedspora_runner import LinkedInClient  # @UnusedImport
 
 
 def read_config_file(filename):
@@ -26,13 +26,16 @@ def read_config_file(filename):
         error = format(excpt)
     raise Exception("Couldn't load config file "+filename+":\n"+error)
 
+
 if __name__ == '__main__':
     config = read_config_file('feedspora.yml')
     feedspora = FeedSpora()
     feedspora.set_feed_urls(config['feeds'])
+
     def connect_account(account):
         '''
-        Initialize a client for the specified account, and register it in FeedSpora
+        Initialize a client for the specified account
+        Then register it in FeedSpora
         :param account:
         '''
         try:
@@ -42,9 +45,9 @@ if __name__ == '__main__':
             feedspora.connect(client)
         except Exception as e:
             logging.error('Cannot connect {} : {}'.format(account['name'],
-                                                        str(e)))
+                                                          str(e)))
     for account in config['accounts']:
-        if not 'enabled' in account or account['enabled']:
+        if 'enabled' not in account or account['enabled']:
             connect_account(account)
     feedspora.set_db_file('feedspora.db')
     feedspora.run()
