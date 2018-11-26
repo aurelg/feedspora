adec863a06452d58ec561166ba153cf173164dab
deej Howard <deej@wildkidz.com>
2018-11-26 12:14:43 -0700

    Re-implement against upstream dev

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,75 +1,72 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
 
-pass_or_fail=0
+PASSORFAIL=0
 function execute_tests() {
-  test_dir=$1
-  for config in "${test_dir}"/*.yml; do
-    filename=$(basename -- "$config")
-    test="${filename%.*}"
-    echo "    $test..."
-    rm -f "${test_dir}"/"${test}".{db,out,log,,err,diff}
-    (cd "${test_dir}" && python -m feedspora --testing "$test" > "${test}".out 2> "${test}".log)
+  TESTDIR="${1:-}"
+  for CONFIG in "${TESTDIR}"/*.yml; do
+    BASEFILENAME=$(basename -s .yml -- "$CONFIG")
+    echo "    $BASEFILENAME..."
+    rm -f "$TESTDIR"/"$BASEFILENAME".{db,out,log,,err,diff}
+    (
+      cd "$TESTDIR" &&
+        python -m feedspora --testing "$BASEFILENAME" \
+          >"$BASEFILENAME".out \
+          2>"$BASEFILENAME".log
+    )
     # We actually hope that the grep DOES fail!
     # Regardless, we don't want the result of grep or diff operations
     # to stop test processing
     set +e 
-    test_err=$(grep 'ERROR:' "${test_dir}"/"${test}".log)
-    if [ "X${test_err}" != "X" ]; then
-      echo "        Execution errors encountered: see ${test_dir}/${test}.err"
-      err_out="${test_dir}/${test}.err"
-      echo "Execution errors (from ${test_dir}/${test}.log):" > "${err_out}"
-      echo "${test_err}" >> "${err_out}"
-      pass_or_fail=1
+    TESTERR=$(grep 'ERROR:' "${TESTDIR}"/"${BASEFILENAME}".log)
+    if [ "X${TESTERR}" != "X" ]; then
+      echo "        Execution errors encountered: see ${TESTDIR}/${BASEFILENAME}.err"
+      ERROUT="${TESTDIR}/${BASEFILENAME}.err"
+      echo "Execution errors (from ${TESTDIR}/${BASEFILENAME}.log):" > "${ERROUT}"
+      echo "${TESTERR}" >> "${ERROUT}"
+      PASSORFAIL=1
     fi
-    test_diff=$(diff "${test_dir}"/"${test}".{au,out})
-    if [ "X${test_diff}" != "X" ]; then
-      diff_out="${test_dir}/${test}.diff"
-      echo "        Output variance detected: see ${diff_out}"
-      echo "Output variance between ${test_dir}/${test}.{au,out}):" > "${diff_out}"
-      echo "${test_diff}" >> "${diff_out}"
-      pass_or_fail=1
+    TESTDIFF=$(diff "${TESTDIR}"/"${BASEFILENAME}".{au,out})
+    if [ "X${TESTDIFF}" != "X" ]; then
+      DIFFOUT="${TESTDIR}/${BASEFILENAME}.diff"
+      echo "        Output variance detected: see ${DIFFOUT}"
+      echo "Output variance between ${TESTDIR}/${BASEFILENAME}.{au,out}):" > "${DIFFOUT}"
+      echo "${TESTDIFF}" >> "${DIFFOUT}"
+      PASSORFAIL=1
     fi
     set -e
   done
 }
 
-test_feed=0
-test_post=0
-show_usage=0
-if [ "X${1:-}" = "X" ]; then
-  echo "No tests specified - aborting"
-  show_usage=1
-elif [ "${1:-}" = "all" ]; then
+case "${1:-}" in
+all)
   echo "Executing all tests"
-  test_feed=1
-  test_post=1
-elif [ "${1:-}" = "feed" ]; then
-  test_feed=1
-elif [ "${1:-}" = "post" ]; then
-  test_post=1
-else
-  echo "Unknown test specification '${1:-}' - aborting"
-  show_usage=1
-fi
-if [ $show_usage == 1 ]; then
-  echo "$0 all | feed | post"
-  exit 1
-fi
-if [ $test_feed == 1 ]; then
   echo "Executing feed tests"
   execute_tests "test_feed"
-fi
-if [ $test_post == 1 ]; then
   echo "Executing post tests"
   execute_tests "test_post"
-fi
+  ;;
+feed)
+  echo "Executing feed tests"
+  execute_tests "test_feed"
+  ;;
+post)
+  echo "Executing post tests"
+  execute_tests "test_post"
+  ;;
+*)
+  echo "No or Unknown test specification '${1:-}' - aborting"
+  echo "${0:-} all | feed | post"
+  exit 1
+  ;;
+esac
+
 echo "All specified tests executed"
-if [ $pass_or_fail == 1 ]; then
+if [ $PASSORFAIL == 1 ]; then
   echo "FAILED"
 else
   echo "PASSED"
 fi
-exit $pass_or_fail
+exit $PASSORFAIL
 
